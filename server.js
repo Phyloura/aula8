e409f3a6173c0a381c3d78f8b9ff2e11a196cda8
Phyloura <r6453670@gmail.com>
2026-04-17 17:34:19 -0300

    Commit 17/04

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 import http from 'node:http';
 import fs from 'node:fs/promises';
 
-
-
 const server = http.createServer(async (req, res) => {
     const {url, method} = req;
     if(url == '/' && method == 'GET'){
         const template = await fs.readFile('./index.html', 'utf-8');
+
+        const dadosBacon = await fs.readFile('inventario.json', 'utf-8');
+        const inventario = JSON.parse(dadosBacon);
+        console.log('Dados carregados: ', inventario);
+
+        const linhasTabela = inventario.map(item => `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.nome}</td>
+                <td>${item.categoria}</td>
+                <td>${item.quantidade}</td>
+            </tr>
+        `).join('');
+
+        const paginaFinal = template.replace('{{TABELA}}', linhasTabela);
+
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
         res.writeHead(200);
-        res.end(template)
+        res.end(paginaFinal);
 
     }
 
